9cd808891e5ff65b05c6ceec6341c300e8757373
Collin Stedman <cstedman@princeton>
2014-03-29 04:10:00 -0400

    Added the bot, everything works

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Autocracy
 ## Join me in taking over Twitch Plays Pokemon.
 
 #### To start
+###### Kings:
 `npm install express`
   
 `node app.js`
+
+###### Peasants
+`./serf.sh`
---
+++ serf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+# A bot which dutifully spams commands to the Twitch Plays Pokemon channel
+
+if [ -a twitch.input ]
+then
+  head -n 3 twitch.input > temp
+  mv temp twitch.input
+else
+  touch twitch.input
+  echo "Please provide your twitch API key. This key will not be given to anybody, even ourselves."
+  read key
+  echo "PASS $key" >> twitch.input # password
+  echo "Please provide your twitch username. Again, this information will remain private on your own machine."
+  read name
+  echo "NICK $name" >> twitch.input # what nick
+  echo "JOIN #twitchplayspokemon" >> twitch.input # what channel
+fi
+
+tail -f twitch.input | telnet 199.9.252.26 6667 | while true; do # what network
+input="$(curl -k https://nodenexus.com:9001/orders)"
+echo "PRIVMSG #twitchplayspokemon $input" >> twitch.input # what to say.
+sleep 31 # number of seconds to wait before saying it again
+done
